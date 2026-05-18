efcc77d274381b19e02d3a8d1fe5b69cdca96988
Your Name <you@example.com>
2026-05-18 14:16:05 +0530

    initial commit2

--- oop.py
+++ oop.py
@@ -1,9 +1,9 @@
-class person:
- def __init__(self,name,age):
-    self.name = name
-    self.age = age
+# class person:
+#  def __init__(self,name,age):
+#     self.name = name
+#     self.age = age
 
-p1 = person("emil",36)
+# p1 = person("emil",36)
 
-print(p1.name)
-print(p1.age)
+# print(p1.name)
+# print(p1.age)
